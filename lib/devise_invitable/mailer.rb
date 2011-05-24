6b0b642eaa6f5ade4968f1084ca31b5ea9501975
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2011-05-24 15:50:46 -0400

    Bug fix

--- lib/devise_invitable/mailer.rb
+++ lib/devise_invitable/mailer.rb
@@ -4,13 +4,12 @@ module DeviseInvitable
     # Deliver an invitation email
     def invitation(record, action)
       initialize_from_record(record)
-      mail {
-        :subject       => translate(devise_mapping, action),
-        :from          => mailer_sender(devise_mapping),
-        :to            => resource.email,
-        :template_path => template_paths,
-        :template      => action
-      }
+
+      mail  :subject       => translate(devise_mapping, action),
+            :from          => mailer_sender(devise_mapping),
+            :to            => resource.email,
+            :template_path => template_paths,
+            :template      => action
     end
   end
 end
