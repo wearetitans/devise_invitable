c6df5d16dd540f31413fceaa61dffbcc4b4ce7ca
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2011-05-24 15:21:28 -0400

    Manually set the template header.

--- lib/devise_invitable/mailer.rb
+++ lib/devise_invitable/mailer.rb
@@ -3,58 +3,15 @@ module DeviseInvitable
     
     # Deliver an invitation email
     def invitation(record, template)
-      setup_mail(record, template)
+      initialize_from_record(record)
+      puts "I just got called"
+      mail {
+        :subject       => translate(devise_mapping, action),
+        :from          => mailer_sender(devise_mapping),
+        :to            => resource.email,
+        :template_path => template_paths,
+        :template      => action
+      }
     end
-  
-  private
-
-  # Configure default email options
-  def setup_mail(record, action)
-    initialize_from_record(record)
-    mail headers_for(action)
-  end
-
-  def initialize_from_record(record)
-    @scope_name = Devise::Mapping.find_scope!(record)
-    @resource   = instance_variable_set("@#{devise_mapping.name}", record)
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[scope_name]
-  end
-
-  def headers_for(action)
-    headers = {
-      :subject       => translate(devise_mapping, action),
-      :from          => mailer_sender(devise_mapping),
-      :to            => resource.email,
-      :template_path => template_paths,
-      :template      => action
-    }
-
-    if resource.respond_to?(:headers_for)
-      headers.merge!(resource.headers_for(action))
-    end
-
-    unless headers.key?(:reply_to)
-      headers[:reply_to] = headers[:from]
-    end
-
-    headers
-  end
-
-  def mailer_sender(mapping)
-    if Devise.mailer_sender.is_a?(Proc)
-      Devise.mailer_sender.call(mapping.name)
-    else
-      Devise.mailer_sender
-    end
-  end
-
-  def template_paths
-    template_path = [self.class.mailer_name]
-    template_path.unshift "#{@devise_mapping.scoped_path}/mailer" if self.class.scoped_views?
-    template_path
-  end
   end
 end
