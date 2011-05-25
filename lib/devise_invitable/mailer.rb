bb3beb5992b0ca708ec9300450aa470e14732459
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2011-05-24 22:24:07 -0400

    Update for a bug fix with mailer rendering the correct template.

--- lib/devise_invitable/mailer.rb
+++ lib/devise_invitable/mailer.rb
@@ -4,12 +4,9 @@ module DeviseInvitable
     # Deliver an invitation email
     def invitation(record, action)
       initialize_from_record(record)
-
-      mail  :subject       => translate(devise_mapping, action),
-            :from          => mailer_sender(devise_mapping),
-            :to            => resource.email,
-            :template_path => template_paths,
-            :template      => action
+      
+      @template = action
+      mail  :subject => translate(devise_mapping, action), :from => mailer_sender(devise_mapping), :to => resource.email, :template_path => template_paths
     end
   end
 end
